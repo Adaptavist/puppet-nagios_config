b8c4d7ef5b932005bc7e8a1a6a2694cfb8816642
Matthew Hope <mhope@adaptavist.com>
2015-12-29 19:40:12 +0000

    seperated custom_plugin define into its own file

---
+++ manifests/custom_plugin.pp
@@ -0,0 +1,39 @@
+
+# Define: nagios::custom_plugin
+define nagios::custom_plugin (
+    $local_path,
+    $file_name,
+    $gitrepo    = 'false',
+    $plugin_url = 'false',
+    $user       = 'root',
+    $group      = 'root',
+    ) {
+    if ($gitrepo != 'false'){
+        exec {
+            "clone custom nagios plugin ${name} from gitrepo":
+                command   => "git archive --format=tar --remote=${gitrepo} HEAD ${file_name} | tar xf -",
+                cwd       => $local_path,
+                logoutput => on_failure,
+                unless    => ["test -f ${local_path}/${file_name}"],
+        }
+        Exec["clone custom nagios plugin ${name} from gitrepo"] -> Exec["change permissions to plugin file ${name}"]
+    } elsif ($plugin_url != 'false') {
+        exec {
+            "wget nagios plugin ${name}":
+            command => "/usr/bin/wget ${plugin_url}",
+            cwd     => $local_path,
+            unless  => ["test -f ${local_path}/${file_name}"],
+        }
+
+        Exec["wget nagios plugin ${name}"] -> Exec["change permissions to plugin file ${name}"]
+
+    } else {
+        fail('You have to provide gitrepo or plugin_url')
+    }
+
+    exec {
+        "change permissions to plugin file ${name}":
+            command   => "chown ${user}:${group} ${local_path}/${file_name}",
+            logoutput => on_failure,
+    }
+}
